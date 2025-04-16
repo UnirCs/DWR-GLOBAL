a35b772fba654b97eedc03836e25ce803013a326
javier.mora.alcazar.contractor <javier.mora.alcazar.contractor@bbva.com>
2025-04-16 18:12:11 +0200

    añadiendo tema 5 y tema 6

---
+++ Tema 6/codigo apoyo/ejemplo 9/index.js
@@ -0,0 +1,5 @@
+// Mostrar el toast al cargar la página
+document.addEventListener('DOMContentLoaded', () => {
+    const toast = new bootstrap.Toast(document.getElementById('toastReserva'));
+    toast.show();
+  });
